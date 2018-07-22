88ca790a36eec8e177b52e1ad870ae8e1dc98522
Joey Thomas <joeypunkrock11@gmail.com>
2018-07-22 15:39:00 +0100

    Built base accordion structure

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,7 +1,11 @@
 import $ from 'jquery';
 import 'bootstrap';
+import 'rangeslider.js';
 import '../scss/style.scss';
 
 $(function () {
     $('[data-toggle="tooltip"]').tooltip();
+    $('.range-slider').rangeslider({
+        polyfill: false
+    });
 })
